56b695c3ae5804389db7688135b117915dc4d95a
aydiaziz <115266706+aydiaziz@users.noreply.github.com>
2025-05-25 10:00:08 +0100

    fd

--- SLFrontend/src/environments/environment.prod.ts
+++ SLFrontend/src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 console.log(">>> PROD ENVIRONMENT LOADED <<<");
+
 export const environment = {
   production: true,
   apiUrl: '/api',
   chatUrl: ''
 };
+console.log("API URL:", environment.apiUrl);
