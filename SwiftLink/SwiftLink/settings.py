11b868edafd2406e7140f4a95954c6c6c9d0b7d1
aydiaziz <115266706+aydiaziz@users.noreply.github.com>
2025-05-26 13:52:47 +0100

    .env

--- SwiftLink/SwiftLink/settings.py
+++ SwiftLink/SwiftLink/settings.py
@@ -99,16 +99,19 @@ WSGI_APPLICATION = 'SwiftLink.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'postgres',  # ← Container name as host
-        'PORT': '5432',
+        'NAME': os.environ.get('DB_NAME', 'postgres'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
+        'HOST': os.environ.get('DB_HOST', 'db'),  
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
