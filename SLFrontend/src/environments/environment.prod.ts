bf5505c57a169c433f433fb4519f3619113d30bf
aydiaziz <115266706+aydiaziz@users.noreply.github.com>
2025-05-26 20:25:38 +0100

    Update environment.prod.ts

--- SLFrontend/src/environments/environment.prod.ts
+++ SLFrontend/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ console.log(">>> PROD ENVIRONMENT LOADED <<<");
 
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8001/api',
+  apiUrl: 'https://www.swift-helpers.com/api',
   chatUrl: ''
 };
 console.log("API URL:", environment.apiUrl);
