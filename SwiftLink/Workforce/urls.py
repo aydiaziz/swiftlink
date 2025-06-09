0bd065460341e2d40b340a1ccdc289bae1dfa337
aydiaziz <115266706+aydiaziz@users.noreply.github.com>
2025-06-09 17:14:36 +0100

    Update urls.py

--- SwiftLink/Workforce/urls.py
+++ SwiftLink/Workforce/urls.py
@@ -1,6 +1,6 @@
 # dashboard/urls.py
 from django.urls import path
-from Workforce.views import dashboard_stats,get_helper_profile, activate_helper,onboard_helper,list_helpers,get_helper_detail,complete_helper_profile
+from Workforce.views import dashboard_stats,get_helper_profile, activate_helper,onboard_helper,list_helpers,get_helper_detail,complete_helper_profile,send_interview_invite
 
 urlpatterns = [
     path('stats/', dashboard_stats, name='dashboard-stats'),
@@ -9,6 +9,6 @@ urlpatterns = [
     path('admin/helpers/<int:helper_id>/accept', onboard_helper),
     path('admin/helper/<int:user_id>', get_helper_detail),
     path('helper/profile-completion/<int:id>/', complete_helper_profile),
-    path('admin/helper/<int:user_id>/send-interview/', get_helper_detail),
+    path('admin/helper/<int:user_id>/send-interview/', send_interview_invite),
     path('helpers/<int:helper_id>/activate/', activate_helper, name='activate_helper'),
 ]
