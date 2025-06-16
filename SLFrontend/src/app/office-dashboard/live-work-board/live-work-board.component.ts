f687286cafd5146b1a2d325e6b24f2aebe1e6ae9
aydiaziz <115266706+aydiaziz@users.noreply.github.com>
2025-06-16 16:28:53 +0100

    a

--- SLFrontend/src/app/office-dashboard/live-work-board/live-work-board.component.ts
+++ SLFrontend/src/app/office-dashboard/live-work-board/live-work-board.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../../services/order.service';
+import { CommonModule, NgFor } from '@angular/common';
 @Component({
   selector: 'app-live-work-board',
   standalone: true,
-  imports: [],
+  imports: [NgFor,CommonModule],
   templateUrl: './live-work-board.component.html',
   styleUrl: './live-work-board.component.css'
 })
-export class LiveWorkBoardComponent {
+export class LiveWorkBoardComponent implements OnInit {
+  orders: any[] = [];
+  errorMessage: string = '';
+  constructor(private orderService: OrderService) {}
 
+  ngOnInit(): void {
+    this.loadOrders();
+  }
+
+  loadOrders(): void {
+    this.orderService.getAllOrders().subscribe({
+      next: (data) => this.orders = data,
+      error: (err) => console.error('Error loading orders:', err)
+    });
+  }
+  // ✅ Liker un ordre
+  likeOrder(orderID: number): void {
+    this.orderService.likeOrder(orderID).subscribe({
+      next: () => {
+        
+        this.loadOrders();  // 🔄 Rafraîchir les ordres après le like
+      },
+      error: (err) => {
+        console.error('Error liking order:', err);
+        this.errorMessage = 'Failed to like order. Please try again!';
+      }
+    });
+  }
+  getPriorityClass(priority: string): string {
+    switch (priority.toLowerCase()) {
+      case 'low': return 'low';
+      case 'medium': return 'medium';
+      case 'high': return 'high';
+      default: return '';
+    }
+  }
+  
 }
