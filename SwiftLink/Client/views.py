d37cd295562203410dce46f5a8b16db3cf2cc3aa
aydiaziz <115266706+aydiaziz@users.noreply.github.com>
2025-06-17 10:52:55 +0100

    Add membership API

--- SwiftLink/Client/views.py
+++ SwiftLink/Client/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from .models import Membership
+from .serializers import MembershipSerializer
 
 # Create your views here.
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_memberships(request):
+    memberships = Membership.objects.all()
+    serializer = MembershipSerializer(memberships, many=True)
+    return Response(serializer.data)
